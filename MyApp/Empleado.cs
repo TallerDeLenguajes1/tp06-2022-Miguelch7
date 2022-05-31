9533a72ea3003c5fd7ae9b91d822d4151a8f1f66
Miguel <chanampamiguel@gmail.com>
2022-05-31 11:50:45 -0300

    Crear metodos clase Empleado

--- MyApp/Empleado.cs
+++ MyApp/Empleado.cs
@@ -1,12 +1,12 @@
 public class Empleado {
-    public string Nombre = "";
-    public string Apellido = "";
-    public DateTime FechaNac;
-    public char EstadoCivil;
-    public char Genero;
-    public DateTime FechaIngreso = new DateTime();
-    public double SueldoBasico;
-    public CargoEnum Cargo;
+    private string Nombre;
+    private string Apellido;
+    private DateTime FechaNac;
+    private char EstadoCivil;
+    private char Genero;
+    private DateTime FechaIngreso = new DateTime();
+    private double SueldoBasico;
+    private CargoEnum Cargo;
 
     public Empleado(string nombre, string apellido, DateTime fechaNac, char estadoCivil, char genero, DateTime fechaIngreso, double sueldoBasico, CargoEnum cargo) {
         Nombre = nombre;
@@ -18,4 +18,74 @@ public class Empleado {
         SueldoBasico = sueldoBasico;
         Cargo = cargo;
     }
-}
+
+    public int Antiguedad() {
+        int antiguedad = DateTime.Now.Year - FechaIngreso.Year;
+
+        if (DateTime.Now.Month <= FechaIngreso.Month && DateTime.Now.Day <= FechaIngreso.Day) {
+            antiguedad--;
+        }
+
+        return antiguedad;
+    }
+
+    public int Edad() {
+        int edad = DateTime.Now.Year - FechaNac.Year;
+
+        if (DateTime.Now.Month <= FechaNac.Month && DateTime.Now.Day <= FechaNac.Day) {
+            edad--;
+        }
+
+        return edad;
+    }
+
+    public int Jubilacion() {
+
+        int edadAJubilarse = 0;
+
+        if (Genero == 'M') {
+            edadAJubilarse = 65;            
+        };
+
+        if (Genero == 'F') {
+            edadAJubilarse = 60;
+        };
+
+        int tiempoRestante = edadAJubilarse - Edad();
+
+        return tiempoRestante;
+    }
+
+    public double Salario() {
+        double adicional = 0;
+
+        if (Antiguedad() <= 20) {
+            adicional = SueldoBasico * 0.01 * Antiguedad();
+        } else {
+            adicional = SueldoBasico * 0.25;
+        };
+        
+        if (Cargo == CargoEnum.Ingeniero || Cargo == CargoEnum.Especialista) {
+            adicional *= .5;
+        };
+
+        if (EstadoCivil == 'C') {
+            adicional += 15000;
+        };
+
+        return SueldoBasico + adicional;
+    }
+
+    public void MostrarEmpleado() {
+        Console.WriteLine($"Empleado: { this.Apellido }, { this.Nombre }");
+        Console.WriteLine("Fecha de nacimiento: " + FechaNac.ToShortDateString());
+        Console.WriteLine("Edad: " + Edad() + " años");
+        Console.WriteLine("Estado Civil: " + EstadoCivil);
+        Console.WriteLine("Género: " + Genero);
+        Console.WriteLine("Fecha de ingreso: " + FechaIngreso.ToShortDateString());
+        Console.WriteLine("Antiguedad: " + Antiguedad() + " años");
+        Console.WriteLine("Sueldo básico: $" + SueldoBasico);
+        Console.WriteLine("Salario: $" + Salario());
+    }
+
+};
