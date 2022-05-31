f896d76095cf646cda233f9da67620c7a028234b
Miguel <chanampamiguel@gmail.com>
2022-05-31 11:51:01 -0300

    Cargar y mostrar empleados

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,45 +1,50 @@
-﻿int operacion, ejecucion = 1;
-double numero;
+﻿Console.WriteLine("========== Ejercicio 2 ==========");
 
-Calculadora calculadora = new Calculadora(0);
+Empleado[] empleados = new Empleado[3];
 
-do
-{
-    do
-    {
-        Console.Write("Elija una operación a realizar: (1: Sumar | 2: Restar | 3: Multiplicación | 4: División | 5: Limpiar): ");
-        operacion = Convert.ToInt32(Console.ReadLine());
-    } while (operacion < 1 || operacion > 5);
+// Carga empleados
+empleados[0] = new Empleado("Enzo", "Martinez", new DateTime(1978, 2, 15), 'C', 'M', new DateTime(2012, 6 , 10), 80000, CargoEnum.Ingeniero);
+empleados[1] = new Empleado("Luis", "Sanchez", new DateTime(1991, 10, 1), 'S', 'M', new DateTime(2015, 11 , 16), 72000, CargoEnum.Especialista);
+empleados[2] = new Empleado("Clara", "Fernandez", new DateTime(1989, 6, 30), 'S', 'F', new DateTime(2018, 2 , 22), 65000, CargoEnum.Administrativo);
 
-    Console.Write("Ingrese un número: ");
-    numero = Convert.ToDouble(Console.ReadLine());
+// Mostrar empleados
+mostrarTodosLosEmpleados(empleados);
 
-    switch (operacion)
-    {
-        case 1:
-            calculadora.Sumar(numero);
-            Console.WriteLine($"El resultado de sumar { numero } es: { calculadora.Resultado }");
-            break;
-        case 2:
-            calculadora.Restar(numero);
-            Console.WriteLine($"El resultado de restar { numero } es: { calculadora.Resultado }");
-            break;
-        case 3:
-            calculadora.Multiplicar(numero);
-            Console.WriteLine($"El resultado de multiplicar { numero } es: { calculadora.Resultado }");
-            break;
-        case 4:
-            calculadora.Dividir(numero);
-            Console.WriteLine($"El resultado de dividir { numero } es: { calculadora.Resultado }");
-            break;
-        case 5:
-            calculadora.Limpiar();
-            Console.WriteLine($"El resultado de limpiar es: { calculadora.Resultado }");
-            break;
-        default:
-            break;
+// Total salarios
+Console.WriteLine("\nEl monto total de los salarios es: $" + totalSalarios(empleados));
+
+// Datos empleado más proximo a jubilarse
+Console.WriteLine("\nEl empleado más proximo a jubilarse es: ");
+Empleado empleadoMasAntiguo = obtenerEmpleadoMasAntiguo(empleados);
+empleadoMasAntiguo.MostrarEmpleado();
+
+// Funciones
+void mostrarTodosLosEmpleados(Empleado[] empleados) {
+    for (int i = 0; i < empleados.Length; i++) {
+        Console.WriteLine($"----- Empleado { i + 1 } -----");
+        empleados[i].MostrarEmpleado();   
+        Console.WriteLine("");
+    };
+};
+
+double totalSalarios(Empleado[] empleados) {
+    double total = 0;
+
+    foreach (Empleado empleado in empleados) {
+        total += empleado.Salario();
     };
 
-    Console.WriteLine("Quiere seguir operando? (1: Si | 0: No)");
-    ejecucion = Convert.ToInt32(Console.ReadLine());
-} while (ejecucion == 1);
+    return total;
+};
+
+Empleado obtenerEmpleadoMasAntiguo(Empleado[] empleados) {
+    Empleado empleado = empleados[0];
+
+    for (int i = 1; i < empleados.Length; i++) {
+        if (empleados[i].Antiguedad() > empleado.Antiguedad()) {
+           empleado = empleados[i]; 
+        };
+    };
+
+    return empleado;
+};
